ad2ebafb33807223cb52e05137a11da2dba7651d
McSunRise <sunrise22813@gmail.com>
2025-06-10 13:02:49 +0300

    Redis and Docker setup

--- weatherapp/tests.py
+++ weatherapp/tests.py
@@ -1,3 +1,13 @@
+from django.core.cache import cache
 from django.test import TestCase
+from django.core.cache.backends.base import InvalidCacheBackendError
 
-# Create your tests here.
+
+class RedisCacheConnectionTest(TestCase):
+    def test_redis_cache_connection(self):
+        try:
+            cache.set('redis_test_key', 'connected', timeout=5)
+            value = cache.get('redis_test_key')
+            self.assertEqual(value, 'connected', "Redis не возвращает ожидаемое значение")
+        except (ConnectionError, InvalidCacheBackendError) as e:
+            self.fail(f"Redis не подключён или произошла ошибка при доступе к кэшу: {e}")
--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -19,19 +19,17 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open("../private.pem", "r") as key:
+with open("private.pem", "r") as key:
     SECRET_KEY = key.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 # Application definition
 
@@ -42,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'weatherapp'
 ]
 
 MIDDLEWARE = [
@@ -73,17 +72,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'weatherproject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD')
     }
 }
 
+# Caching
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': f'redis://{os.environ.get("REDIS_HOST")}:{os.environ.get("REDIS_PORT")}'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -103,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -114,7 +122,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
