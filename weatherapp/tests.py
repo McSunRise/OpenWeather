62398bf4df5e919e37d74e744e99fc1ddce5ff12
McSunRise <70936443+McSunRise@users.noreply.github.com>
2025-06-15 19:43:49 +0300

    Added logging config (#1)

--- weatherapp/tests.py
+++ weatherapp/tests.py
@@ -1,6 +1,8 @@
 from django.core.cache import cache
 from django.test import TestCase
 from django.core.cache.backends.base import InvalidCacheBackendError
+import os
+import logging
 
 
 class RedisCacheConnectionTest(TestCase):
@@ -11,3 +13,15 @@ class RedisCacheConnectionTest(TestCase):
             self.assertEqual(value, 'connected', "Redis не возвращает ожидаемое значение")
         except (ConnectionError, InvalidCacheBackendError) as e:
             self.fail(f"Redis не подключён или произошла ошибка при доступе к кэшу: {e}")
+
+
+class LogExistenceTest(TestCase):
+    def test_log_existence(self):
+        log_file_path = 'logs/django.log'
+        logger = logging.getLogger('django')
+        logger.info('Log existence test message')
+        self.assertTrue(os.path.exists(log_file_path), 'Log file was not created.')
+        with open(log_file_path, 'r') as f:
+            content = f.read()
+            self.assertIn('Log existence test message', content)
+
